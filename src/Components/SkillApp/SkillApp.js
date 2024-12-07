c74824ec63c5d1fde36220bf4c50adeb8f52a54b
aansd <septiyan63@gmail.com>
2024-12-07 10:05:01 +0700

    update 2.2

--- src/Components/SkillApp/SkillApp.js
+++ src/Components/SkillApp/SkillApp.js
@@ -1,7 +1,7 @@
 import React from 'react'
 const SkillApp = () => {
   return (
-    <div className='skill w-full lg:w-[1024px] h-[365px] mx-auto lg:mt-60 md:mt-[600px] mt-[10px] px-6 lg:mb-30 mb-40'>
+    <div className='skill w-full lg:w-[1024px] h-[365px] mx-auto lg:mt-60 md:mt-[600px] mt-[10px] px-6 lg:mb-30 mb-96'>
       <div className='container'>
         <div className='flex'>
         <h1 className="text-cyan-700 lg:text-[32px]">#</h1>
@@ -17,7 +17,10 @@ const SkillApp = () => {
                 <h1 className='text-center text-slate-400'>Langue</h1>
                 </div>
                <div className='border border-t-0 p-2'>
-                <p className=' text-slate-400'>javascript <br/> php</p>
+                <p className=' text-slate-400'> <div className='flex mb-2'><img src='/images/js.png' className='w-[30px] mr-3'/> javascript </div>
+                   
+                  <div className='flex'> <img src='/images/php.png' className='w-[30px] mr-3'/> PHP</div>
+                  </p>
                 </div>
             </div>
             </div>
@@ -27,7 +30,10 @@ const SkillApp = () => {
                 <h1 className='text-center text-slate-400'>Database</h1>
                 </div>
                 <div className='border border-t-0 p-2'>
-                <p className='text-slate-400'>MongoDB <br/> Mysql</p>
+                <p className='text-slate-400'>
+                   <div className='flex mb-2'> <img src='/images/mongo.png' className='w-[30px] mr-3'/> MongoDB</div>
+                   <div className='flex'> <img src='/images/mysql.png' className='w-[30px] mr-3'/> Mysql</div>
+                   </p>
                 </div>
             </div>
            <div className='mt-5'>
@@ -35,17 +41,28 @@ const SkillApp = () => {
                 <h1 className='text-center text-slate-400'>Other</h1>
                 </div>
                 <div className='border border-t-0 p-2'>
-                <p className=' text-slate-400'>HTML CSS <br/> SCSS </p>
+                <p className='text-slate-400'>
+                <div className='flex mb-2'> <img src='/images/html.png' className='w-[30px] mr-3'/> HTML</div>
+                <div className='flex mb-2'> <img src='/images/css.png' className='w-[30px] mr-3'/> CSS</div>
+                <div className='flex mb-2'> <img src='/images/scss.png' className='w-[30px] mr-3'/> SCSS</div>
+                </p>
                 </div>
             </div>
            </div>
-           <div className='lg:mt-0 mt-[-107px]'>
+           <div className='lg:mt-0 mt-[-172px]'>
            <div className=''>
             <div className='border'>
                 <h1 className='text-center text-slate-400'>Tools</h1>
                 </div>
                 <div className='border border-t-0 p-2'>
-                <p className='text-slate-400'>VScode Git Laragon bootstrap Tailwind node.js</p>
+                <p className='text-slate-400'>
+                <div className='flex mb-2'> <img src='/images/vs.png' className='w-[30px] mr-3'/> Visual Studio</div>
+                <div className='flex mb-2'> <img src='/images/git.png' className='w-[30px] mr-3'/> Git</div>
+                <div className='flex mb-2'> <img src='/images/laragon.png' className='w-[30px] mr-3'/>Laragon</div>
+                <div className='flex mb-2'> <img src='/images/boostsrap.png' className='w-[30px] mr-3'/>Bootstrap</div>
+                <div className='flex mb-2'> <img src='/images/tailwind.png' className='w-[30px] mr-3'/> Tailwind</div>
+                <div className='flex mb-2'> <img src='/images/nodejs.png' className='w-[30px] mr-3'/> NodeJs</div>
+                </p>
                 </div>
             </div>
            <div className=' mt-5'>
@@ -53,7 +70,12 @@ const SkillApp = () => {
                 <h1 className='text-center text-slate-400'>framework</h1>
                 </div>
                 <div className='border border-t-0 p-2'>
-                <p className=' text-slate-400'>React.js Express.js Vue.js Laravel</p>
+                <p className=' text-slate-400'>
+                <div className='flex mb-2'> <img src='/images/react.png' className='w-[30px] mr-3'/> ReactJs</div>
+                <div className='flex mb-2'> <img src='/images/expres.png' className='w-[30px] mr-3 '/>ExpressJs</div>
+                <div className='flex mb-2'> <img src='/images/vue.png' className='w-[30px] mr-3'/>VueJs</div>
+                <div className='flex mb-2'> <img src='/images/laravel.png' className='w-[30px] mr-3'/>Laravel</div>
+                </p>
                 </div>
             </div>
            </div>
